6cf09afd92b01e68da51e785bae4306067c846c0
Coy Sellers <coysellers@gmail.com>
2018-02-26 07:40:49 -0600

    Add connect method for progressive enhancement

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -3,6 +3,12 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
     static targets = ['source']
 
+    connect() {
+        if (document.queryCommandSupported("copy")) {
+            this.element.classList.add("clipboard--supported")
+        }
+    }
+
     copy() {
         this.sourceTarget.select()
         document.execCommand('copy')
